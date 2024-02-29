dee5b06eaa7559d81dfeae6f4869b4003494135b
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-02-29 14:47:03 +0100

    controller wrappers

---
+++ bbbqd/wrappers/controller_wrappers.py
@@ -0,0 +1,23 @@
+from bbbqd.wrappers.action_wrappers import *
+from bbbqd.wrappers.observation_wrappers import *
+
+
+class ControllerWrapper(gym.Wrapper[EvoGymBase[ObsType, ActType]]):
+    def __init__(self, env: EvoGymBase, skip: int = 0):
+        super().__init__(env)
+        self.env = ActionSpaceCorrectionWrapper(env)
+        if skip > 0:
+            self.env = ActionSkipWrapper(self.env, skip)
+
+
+class GlobalWrapper(ControllerWrapper):
+
+    def __init__(self, env: EvoGymBase, skip: int = 0, **kwargs):
+        super().__init__(env, skip)
+        self.env = GlobalObservationWrapper(GlobalActionWrapper(self.env), **kwargs)
+
+
+class LocalWrapper(ControllerWrapper):
+    def __init__(self, env: EvoGymBase, skip: int = 0, **kwargs):
+        super().__init__(env, skip)
+        self.env = LocalObservationWrapper(LocalActionWrapper(self.env), **kwargs)
