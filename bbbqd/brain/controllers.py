34f54d230d412e09acdab0d3c32287e86aed8154
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-04 15:55:45 +0100

    controllers update

--- bbbqd/brain/controllers.py
+++ bbbqd/brain/controllers.py
@@ -1,11 +1,14 @@
 import numpy as np
-from typing import Callable, List
+from typing import Callable, List, Union
 
 
 class Controller:
 
     def __init__(self, control_function: Callable[[np.ndarray], np.ndarray]):
         self.control_function = control_function
+
+    def compute_action(self, observation: Union[np.ndarray, List[np.ndarray]]) -> Union[np.ndarray, List[np.ndarray]]:
+        raise NotImplementedError
 
 
 class GlobalController(Controller):
