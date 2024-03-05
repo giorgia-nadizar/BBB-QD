f7d20452f006eae8782f9452a0e4cb2d9da21ed5
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-05 17:28:48 +0100

    make env added

--- bbbqd/wrappers/__init__.py
+++ bbbqd/wrappers/__init__.py
@@ -1,1 +1,17 @@
 # taken from https://github.com/mertan-a/gecco-23
+from __future__ import annotations
+
+from typing import Union
+
+from bbbqd.wrappers.action_wrappers import *
+from bbbqd.wrappers.full_wrappers import FullWrapper, GlobalWrapper, LocalWrapper
+from bbbqd.wrappers.observation_wrappers import *
+
+
+def make_env(config: Dict[str, Any]) -> Union[gym.Env, FullWrapper]:
+    env = gym.make(config["env_name"], body=np.array(config["body"]))
+    if config["controller"] == "global":
+        env = GlobalWrapper(env, **config["flags"])
+    elif config["controller"] == 'local':
+        env = LocalWrapper(env, **config["flags"])
+    return env
--- bbbqd/wrappers/full_wrappers.py
+++ bbbqd/wrappers/full_wrappers.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from bbbqd.wrappers.action_wrappers import *
 from bbbqd.wrappers.observation_wrappers import *
 
