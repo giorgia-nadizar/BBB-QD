eca642510867f8daf05172e8f6470301731e5cd9
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-05 17:58:31 +0100

    actions rescaled wrt action space

--- bbbqd/wrappers/action_wrappers.py
+++ bbbqd/wrappers/action_wrappers.py
@@ -18,15 +18,26 @@ class ActionSpaceCorrectionWrapper(gym.Wrapper):
 class ActionWrapper(gym.Wrapper):
     """ base class for action wrappers """
 
-    def __init__(self, env: EvoGymBase):
+    def __init__(self, env: EvoGymBase, rescale_action: bool = True):
         super().__init__(env)
         self.robot_structure = env.world.objects['robot'].get_structure()
         self.active_voxels = self.robot_structure == 3
         self.active_voxels += self.robot_structure == 4
         self.robot_bounding_box = self.env.world.objects['robot'].get_structure().shape
+        self.rescale = rescale_action
+
+    def rescale_action(self, action: np.ndarray) -> np.ndarray:
+        if self.rescale:
+            low, high = self.env.action_space.low[0], self.env.action_space.high[0]
+            normalized_action = action / 2 + .5
+            rescaled_action = normalized_action * (high - low) + low
+            return rescaled_action
+        else:
+            return action
 
 
 # noinspection PyUnresolvedReferences
+
 class LocalActionWrapper(ActionWrapper):
     """ flattens the action before feeding it to the inner step method: it can be fed in a 2d array """
 
@@ -42,7 +53,8 @@ class LocalActionWrapper(ActionWrapper):
     # takes actions only for the active voxels (can be fed in a 1D or 2D array)
     def step(self, action: np.ndarray) -> Tuple[ObsType, float, bool, Dict[str, Any]]:
         action = action.flatten()
-        obs, reward, done, info = self.env.step(action)
+        rescaled_action = self.rescale_action(action)
+        obs, reward, done, info = self.env.step(rescaled_action)
         if 'rl' in self.kwargs:
             reward = np.array([reward] * self.active_voxels.sum())
             done = np.array([done] * self.active_voxels.sum())
@@ -51,6 +63,7 @@ class LocalActionWrapper(ActionWrapper):
 
 
 # noinspection PyUnresolvedReferences
+
 class GlobalActionWrapper(ActionWrapper):
     """ only takes the values computed for the voxels that are actually active (it expects a number of actions
     matching the grid size, regardless of where voxels are) """
@@ -66,7 +79,8 @@ class GlobalActionWrapper(ActionWrapper):
     # takes actions for all voxels and uses only the needed ones
     def step(self, action: np.ndarray) -> Tuple[ObsType, float, bool, Dict[str, Any]]:
         action = action[self.active_voxels.flatten()]
-        obs, reward, done, info = self.env.step(action)
+        rescaled_action = self.rescale_action(action)
+        obs, reward, done, info = self.env.step(rescaled_action)
         return obs, reward, done, info
 
 
---
+++ examples/cgp_control_tester.py
@@ -0,0 +1,89 @@
+from functools import partial
+from multiprocessing import Pool
+
+import jax
+import jax.numpy as jnp
+
+from bbbqd.brain.controllers import compute_controller_generation_fn
+from bbbqd.core.evaluation import evaluate_controller
+from bbbqd.wrappers import make_env
+from qdax.core.gp.encoding import compute_encoding_function
+from qdax.core.gp.individual import compute_genome_mask, compute_mutation_mask, generate_population
+from qdax.core.gp.utils import update_config
+
+config = {
+    "n_nodes": 50,
+    "p_mut_inputs": 0.1,
+    "p_mut_functions": 0.1,
+    "p_mut_outputs": 0.3,
+    "solver": "cgp",
+    "env_name": "Walker-v0",
+    "body": [[0, 0, 0], [3, 3, 3], [3, 0, 3]],
+    "episode_length": 200,
+    "pop_size": 100,
+    "parents_size": 100,
+    "n_iterations": 100,
+    "seed": 0,
+    "fixed_outputs": True,
+    "controller": "global",
+    "flags": {
+        "observe_voxel_vel": True,
+        "observe_voxel_volume": True,
+        "observe_time": True,
+    },
+    "jax": True,
+    "program_wrapper": True
+}
+
+# Create environment with wrappers
+env = make_env(config)
+
+# Update config with env info
+config = update_config(config, env)
+
+# Init a random key
+random_key = jax.random.PRNGKey(config["seed"])
+
+# Init population of controllers
+genome_mask = compute_genome_mask(config, config["n_in"], config["n_out"])
+mutation_mask = compute_mutation_mask(config, config["n_out"])
+random_key, pop_key = jax.random.split(random_key)
+if config.get("fixed_outputs", False):
+    fixed_outputs = jnp.arange(start=config["buffer_size"] - config["n_out"], stop=config["buffer_size"], step=1)
+    population = generate_population(
+        pop_size=config["parents_size"],
+        genome_mask=genome_mask,
+        rnd_key=pop_key,
+        fixed_genome_trailing=fixed_outputs
+    )
+    config["p_mut_outputs"] = 0
+else:
+    population = generate_population(pop_size=config["parents_size"], genome_mask=genome_mask, rnd_key=pop_key)
+
+# Define encoding function
+encoding_fn = compute_encoding_function(config)
+
+# Define function to return the proper controller
+controller_creation_fn = compute_controller_generation_fn(config)
+
+# Create controller evaluation function
+controller_evaluation_fn = partial(evaluate_controller, config=config)
+
+
+def _evaluate_genome(genome: jnp.ndarray) -> float:
+    return controller_evaluation_fn(controller_creation_fn(encoding_fn(genome)))
+
+
+genomes_list = [g for g in population]
+
+pool_size = config.get("pool_size", config["population_size"])
+
+fitnesses = []
+start_idx = 0
+with Pool(pool_size) as p:
+    current_genomes = genomes_list[start_idx:min(start_idx + pool_size, len(genomes_list))]
+    current_fitnesses = p.map(_evaluate_genome, current_genomes)
+    fitnesses = fitnesses + current_fitnesses
+    start_idx += pool_size
+
+print(fitnesses)
