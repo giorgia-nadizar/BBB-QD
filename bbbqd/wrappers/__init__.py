f7089a0b8d95ead19724608f3eb120281a8e327b
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-06 10:00:30 +0100

    takes body as param

--- bbbqd/wrappers/__init__.py
+++ bbbqd/wrappers/__init__.py
@@ -3,13 +3,17 @@ from __future__ import annotations
 
 from typing import Union
 
+import numpy as np
+
 from bbbqd.wrappers.action_wrappers import *
 from bbbqd.wrappers.full_wrappers import FullWrapper, GlobalWrapper, LocalWrapper
 from bbbqd.wrappers.observation_wrappers import *
 
 
-def make_env(config: Dict[str, Any]) -> Union[gym.Env, FullWrapper]:
-    env = gym.make(config["env_name"], body=np.array(config["body"]))
+def make_env(config: Dict[str, Any], body: np.ndarray = None) -> Union[gym.Env, FullWrapper]:
+    if body is None:
+        body = np.array(config["body"])
+    env = gym.make(config["env_name"], body=body)
     if config["controller"] == "global":
         env = GlobalWrapper(env, **config["flags"])
     elif config["controller"] == 'local':
