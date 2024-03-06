76c64307853557b89980bf4842ad187473d58dc4
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-06 10:01:06 +0100

    example CGP optimization

--- examples/cgp_control_tester.py
+++ examples/cgp_control_tester.py
@@ -1,15 +1,24 @@
+import os
+import time
+from datetime import datetime
 from functools import partial
 from multiprocessing import Pool
 
 import jax
 import jax.numpy as jnp
+from typing import Tuple
 
 from bbbqd.brain.controllers import compute_controller_generation_fn
 from bbbqd.core.evaluation import evaluate_controller
 from bbbqd.wrappers import make_env
+from qdax.baselines.genetic_algorithm import GeneticAlgorithm
+from qdax.core.emitters.standard_emitters import MixingEmitter
 from qdax.core.gp.encoding import compute_encoding_function
-from qdax.core.gp.individual import compute_genome_mask, compute_mutation_mask, generate_population
+from qdax.core.gp.individual import compute_genome_mask, compute_mutation_mask, generate_population, \
+    compute_mutation_fn, compute_variation_mutation_fn
 from qdax.core.gp.utils import update_config
+from qdax.types import RNGKey, Fitness, ExtraScores
+from qdax.utils.metrics import default_ga_metrics, CSVLogger
 
 config = {
     "n_nodes": 50,
@@ -21,18 +30,18 @@ config = {
     "body": [[0, 0, 0], [3, 3, 3], [3, 0, 3]],
     "episode_length": 200,
     "pop_size": 10,
-    "parents_size": 10,
-    "n_iterations": 100,
+    "parents_size": 9,
+    "n_iterations": 20,
     "seed": 0,
     "fixed_outputs": True,
     "controller": "global",
     "flags": {
         "observe_voxel_vel": True,
         "observe_voxel_volume": True,
-        "observe_time": True,
+        "observe_time": False,
     },
     "jax": True,
-    "program_wrapper": True
+    "program_wrapper": True,
 }
 
 # Create environment with wrappers
@@ -70,21 +79,73 @@ controller_creation_fn = compute_controller_generation_fn(config)
 controller_evaluation_fn = partial(evaluate_controller, config=config)
 
 
+# Define genome evaluation fn
 def _evaluate_genome(genome: jnp.ndarray) -> float:
     return controller_evaluation_fn(controller_creation_fn(encoding_fn(genome)))
 
 
-genomes_list = [g for g in population]
+# Define scoring fn
+def _scoring_fn(genomes: jnp.ndarray, rnd_key: RNGKey) -> Tuple[Fitness, ExtraScores, RNGKey]:
+    genomes_list = [g for g in genomes]
+    pool_size = config.get("pool_size", config["pop_size"])
+    fitnesses = []
+    start_idx = 0
+    while start_idx < len(genomes_list):
+        with Pool(pool_size) as p:
+            current_genomes = genomes_list[start_idx:min(start_idx + pool_size, len(genomes_list))]
+            current_fitnesses = p.map(_evaluate_genome, current_genomes)
+            fitnesses = fitnesses + current_fitnesses
+            start_idx += pool_size
 
-pool_size = config.get("pool_size", config["pop_size"])
+    return jnp.expand_dims(jnp.asarray(fitnesses), axis=1), None, rnd_key
 
-fitnesses = []
-start_idx = 0
-while start_idx < len(genomes_list):
-    with Pool(pool_size) as p:
-        current_genomes = genomes_list[start_idx:min(start_idx + pool_size, len(genomes_list))]
-        current_fitnesses = p.map(_evaluate_genome, current_genomes)
-        fitnesses = fitnesses + current_fitnesses
-        start_idx += pool_size
 
-print(fitnesses)
+# Define emitter
+mutation_fn = compute_mutation_fn(genome_mask, mutation_mask)
+variation_fn = None
+variation_perc = 0.0
+if config["solver"] == "lgp":
+    variation_fn = compute_variation_mutation_fn(genome_mask, mutation_mask)
+    variation_perc = 1.0
+
+mixing_emitter = MixingEmitter(
+    mutation_fn=mutation_fn,
+    variation_fn=variation_fn,
+    variation_percentage=variation_perc,
+    batch_size=config["parents_size"]
+)
+
+genetic_algorithm = GeneticAlgorithm(
+    scoring_function=_scoring_fn,
+    emitter=mixing_emitter,
+    metrics_function=default_ga_metrics,
+)
+
+# Compute initial repertoire and emitter state
+repertoire, emitter_state, random_key = genetic_algorithm.init(population, config["pop_size"], random_key)
+
+headers = ["iteration", "max_fitness", "time", "current_time"]
+
+name = f"{config.get('run_name', 'trial')}_{config['seed']}"
+
+csv_logger = CSVLogger(
+    f"../results/{name}.csv",
+    header=headers
+)
+
+for i in range(config["n_iterations"]):
+    start_time = time.time()
+    # main iterations
+    repertoire, emitter_state, metrics, random_key = genetic_algorithm.update(repertoire, emitter_state, random_key)
+    timelapse = time.time() - start_time
+    current_time = datetime.now()
+
+    # log metrics
+    logged_metrics = {"time": timelapse, "iteration": i + 1, "current_time": current_time,
+                      "max_fitness": metrics["max_fitness"][0]}
+
+    csv_logger.log(logged_metrics)
+    print(f"{i}\t{logged_metrics['max_fitness']}")
+
+os.makedirs(f"../results/{name}/", exist_ok=True)
+repertoire.save(f"../results/{name}/")
