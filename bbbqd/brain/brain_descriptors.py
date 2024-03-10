d97d0703b1c8ae943292ff173b2e7e24b669b10f
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-10 10:29:53 +0100

    brain descriptors

---
+++ bbbqd/brain/brain_descriptors.py
@@ -0,0 +1,30 @@
+from functools import partial
+from typing import Dict, Tuple, Callable
+
+import jax.numpy as jnp
+from jax import vmap
+
+from qdax.core.gp.graph_utils import compute_cgp_descriptors, compute_lgp_descriptors
+from qdax.types import Genotype, Descriptor
+
+
+def get_graph_descriptor_extractor(config: Dict) -> Tuple[Callable[[Genotype], Descriptor], int]:
+    indexes = {
+        "complexity": [0],
+        "inputs_usage": [1],
+        "function_arities": [2, 3],
+        "function_types": [4, 5, 6],
+        "function_arities_fraction": [7]
+    }
+    list_of_descriptors = config["graph_descriptors"] if isinstance(config["graph_descriptors"], list) \
+        else [config["graph_descriptors"]]
+    descr_indexes = jnp.asarray([idx for desc_name in list_of_descriptors for idx in indexes[desc_name]])
+    if config["solver"] == "cgp":
+        single_genome_descriptor_function = partial(compute_cgp_descriptors, config=config,
+                                                    descriptors_indexes=descr_indexes)
+    elif config["solver"] == "lgp":
+        single_genome_descriptor_function = partial(compute_lgp_descriptors, config=config,
+                                                    descriptors_indexes=descr_indexes)
+    else:
+        raise ValueError("Solver must be either cgp or lgp.")
+    return vmap(single_genome_descriptor_function), len(descr_indexes)
