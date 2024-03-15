fecda8a7dd881ce737dfdb56a53490faf9ef86e7
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-15 13:39:54 +0100

    descriptors extraction added

--- bbbqd/wrappers/qd_wrappers.py
+++ bbbqd/wrappers/qd_wrappers.py
@@ -6,6 +6,10 @@ from gym.core import ActType, ObsType
 
 
 def get_descriptors_extractor_function(descriptors: List[str]) -> Callable[[Dict[str, Any]], np.ndarray]:
+    existing_descriptors = ["velocity", "position"]
+    for descriptor in descriptors:
+        assert descriptor in existing_descriptors
+
     def _descriptors_extractor(info: Dict[str, Any]) -> np.ndarray:
         descriptors_values = []
         for descriptor in descriptors:
