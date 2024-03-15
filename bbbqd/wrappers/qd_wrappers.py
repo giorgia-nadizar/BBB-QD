d88edd5686f9afb26a850f35f1f1aaeea369e9ec
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-15 12:23:46 +0100

    wrappers for qd

---
+++ bbbqd/wrappers/qd_wrappers.py
@@ -0,0 +1,20 @@
+from typing import Tuple
+
+import gym
+from gym.core import ActType, ObsType
+
+
+class CenterVelocityWrapper(gym.Wrapper):
+
+    def step(self, action: ActType) -> Tuple[ObsType, float, bool, dict]:
+        obs, reward, done, info = super().step(action)
+        info["velocity"] = self.env.get_vel_com_obs("robot")
+        return obs, reward, done, info
+
+
+class CenterPositionWrapper(gym.Wrapper):
+
+    def step(self, action: ActType) -> Tuple[ObsType, float, bool, dict]:
+        obs, reward, done, info = super().step(action)
+        info["position"] = self.env.get_pos_com_obs("robot")
+        return obs, reward, done, info
