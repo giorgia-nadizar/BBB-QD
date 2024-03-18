72e16544fd0a2a4bc04ec89e7d4b0ebb19def95b
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-18 14:59:41 +0100

    polar coordinates added

--- bbbqd/behavior/behavior_utils.py
+++ bbbqd/behavior/behavior_utils.py
@@ -11,12 +11,20 @@ def get_descriptors_functions(config: Dict[str, Any]) -> Tuple[
 
 
 def _get_descriptors_extractor_function(descriptors: List[str]) -> Callable[[Dict[str, Any]], np.ndarray]:
-    existing_descriptors = ["velocity", "position"]
-    for descriptor in descriptors:
-        assert descriptor in existing_descriptors
+    existing_descriptors = ["velocity", "position", "velocity_polar"]
+    for d in descriptors:
+        assert d in existing_descriptors
 
     def _descriptors_extractor(info: Dict[str, Any]) -> np.ndarray:
-        return np.concatenate([info[d] for d in descriptors])
+        descriptors_data = []
+        for descriptor in descriptors:
+            if "polar" in descriptor:
+                x_y = info[descriptor.replace("_polar", "")]
+                descriptors_data.append(np.sqrt(x_y[0] ** 2 + x_y[1] ** 2))
+                descriptors_data.append(np.arctan2(x_y[1], x_y[0]))
+            else:
+                descriptors_data.append(info[descriptor])
+        return np.concatenate(descriptors_data)
 
     return _descriptors_extractor
 
