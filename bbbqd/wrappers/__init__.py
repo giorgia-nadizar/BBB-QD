29ebdc024858348807597a881a0cf049ce3a849c
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-20 13:49:41 +0100

    qd wrappers

--- bbbqd/wrappers/__init__.py
+++ bbbqd/wrappers/__init__.py
@@ -1,4 +1,4 @@
-# taken from https://github.com/mertan-a/gecco-23
+# partially taken from https://github.com/mertan-a/gecco-23
 from __future__ import annotations
 
 from typing import Union
@@ -14,10 +14,22 @@ def make_env(config: Dict[str, Any], body: np.ndarray = None) -> Union[gym.Env, 
             body = np.array(config["body"])
         else:
             body = np.ones((config["grid_size"], config["grid_size"])) * 3
+    qd_wrappers = config.get("qd_wrappers", [])
+    if isinstance(qd_wrappers, str):
+        qd_wrappers = [qd_wrappers]
     env = gym.make(config["env_name"], body=body)
     fixed_body = config.get("fixed_body", False)
     if config["controller"] == "global":
-        env = GlobalWrapper(env, skip=config.get("skip", 0), fixed_body=fixed_body, obs_details=config["flags"])
+        env = GlobalWrapper(env,
+                            skip=config.get("skip", 0),
+                            fixed_body=fixed_body,
+                            obs_details=config["flags"],
+                            qd_wrappers=qd_wrappers
+                            )
     elif config["controller"] == "local":
-        env = LocalWrapper(env, skip=config.get("skip", 0), obs_details=config["flags"])
+        env = LocalWrapper(env,
+                           skip=config.get("skip", 0),
+                           obs_details=config["flags"],
+                           qd_wrappers=qd_wrappers
+                           )
     return env
