23194e0113abaae37d3eb4e8dd18ed449220a938
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-05-07 18:04:37 +0200

    angle walls descriptor

--- bbbqd/wrappers/qd_wrappers.py
+++ bbbqd/wrappers/qd_wrappers.py
@@ -48,10 +48,31 @@ class CenterAngleWrapper(gym.Wrapper):
 
 
 class FloorContactWrapper(gym.Wrapper):
-    floor_level: float = 0.105
+    floor_level: float = 0.1
+    offset: float = 0.005
 
     def step(self, action: ActType) -> Tuple[ObsType, float, bool, dict]:
         obs, reward, done, info = super().step(action)
-        masses_positions = self.env.object_pos_at_time(self.env.get_time(), 'robot')
-        info["floor_contact"] = np.asarray([np.min(masses_positions[1]) <= self.floor_level])
+        masses_positions = self.env.object_pos_at_time(self.env.get_time(), "robot")
+        info["floor_contact"] = np.asarray([np.min(masses_positions[1]) <= (self.floor_level + self.offset)])
+        return obs, reward, done, info
+
+
+class WallsContactWrapper(gym.Wrapper):
+    left_wall: float = 0.1
+    right_wall: float = 0.6
+    offset: float = 0.005
+
+    def step(self, action: ActType) -> Tuple[ObsType, float, bool, dict]:
+        obs, reward, done, info = super().step(action)
+        masses_positions = self.env.object_pos_at_time(self.env.get_time(), "robot")
+        left_contact_ys = masses_positions[1][masses_positions[0] <= (self.left_wall + self.offset)]
+        right_contact_ys = masses_positions[1][masses_positions[0] >= (self.right_wall - self.offset)]
+        if len(left_contact_ys) == 0 or len(right_contact_ys) == 0:
+            info["walls_contact"] = None
+        else:
+            avg_left_contact_y = np.mean(left_contact_ys)
+            avg_right_contact_y = np.mean(right_contact_ys)
+            angle = (avg_right_contact_y - avg_left_contact_y) / (self.right_wall - self.left_wall)
+            info["walls_contact"] = np.abs(angle)
         return obs, reward, done, info
