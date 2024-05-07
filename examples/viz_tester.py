da9713ac4bb7811fd1e921bf1d9b25a4582ec79a
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-05-07 17:15:29 +0200

    file removed

--- examples/viz_tester.py
+++ examples/viz_tester.py
@@ -5,10 +5,12 @@ from evogym import sample_robot
 if __name__ == '__main__':
 
     body, connections = sample_robot((5, 5))
-    env = gym.make('Walker-v0', body=body)
+    env = gym.make('Carrier-v0', body=body)
     env.reset()
 
     for _ in range(1000):
+        print(env.action_space)
+
         action = env.action_space.sample() - 1
         ob, reward, done, info = env.step(action)
         env.render()
