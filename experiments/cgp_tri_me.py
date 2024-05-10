76072cf74003cb2a2524bd89f29ed50932c0e9ce
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-05-10 13:47:54 +0200

    new config

--- experiments/cgp_tri_me.py
+++ experiments/cgp_tri_me.py
@@ -201,9 +201,9 @@ def run_body_evo_me(config: Dict[str, Any]):
 
 if __name__ == '__main__':
     # seeds = range(10)
-    # samplers = ["all", "s1", "s2", "s3"]
-    seeds = [0]
-    samplers = ["all"]
+    samplers = ["all", "s1", "s2", "s3"]
+    seeds = range(5)
+    # samplers = ["all"]
     envs_descriptors = {
         # "Walker-v0": {
         #     "behavior_descriptors": ["velocity_y", "floor_contact"],
