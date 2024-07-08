26455bc31f26d2d874cc2dd2c221137f3ceb1ebb
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-07-08 18:07:30 +0200

    nn connection density descriptors

---
+++ bbbqd/brain/nn_descriptors.py
@@ -0,0 +1,18 @@
+import jax.numpy as jnp
+from flax.core import FrozenDict
+
+
+def count_weights_above_threshold_per_neuron(nn_params: FrozenDict, threshold: float) -> jnp.ndarray:
+    counts = []
+    for layer_name, layer_params in nn_params.items():
+        if 'kernel' in layer_params:
+            kernel = layer_params['kernel']
+            count = jnp.sum(kernel > threshold, axis=1) / kernel.shape[1]
+            counts.append(count)
+    return jnp.concatenate(counts)
+
+
+def mean_and_std_output_connectivity(nn_params: FrozenDict, threshold: float = 0.25) -> jnp.ndarray:
+    weights_above_threshold_per_neuron = count_weights_above_threshold_per_neuron(nn_params["params"], threshold)
+    mean_value, std_value = jnp.mean(weights_above_threshold_per_neuron), jnp.std(weights_above_threshold_per_neuron)
+    return jnp.asarray([mean_value, std_value])
