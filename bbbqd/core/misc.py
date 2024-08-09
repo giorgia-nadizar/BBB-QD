91759c7f20d270d4516b64952b02b48f7c8bdd08
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-08-09 11:23:27 +0200

    combined mutation operator

--- bbbqd/core/misc.py
+++ bbbqd/core/misc.py
@@ -5,6 +5,22 @@ import jax
 from qdax.types import Genotype, RNGKey
 
 
+def gaussian_and_body_mutation(
+        nn_mutation_fn: Callable[[Genotype, RNGKey], Tuple[Genotype, RNGKey]],
+        body_mutation_fn: Callable[[Genotype, RNGKey], Tuple[Genotype, RNGKey]]
+) -> Callable[[Genotype, RNGKey], Tuple[Genotype, RNGKey]]:
+    def _composed_mutation_fn(parent: Genotype, random_key: RNGKey) -> Tuple[Genotype, RNGKey]:
+        nn_genome_parent, body_genome_parent = parent.pop("body")
+        random_key, nn_key = jax.random.split(random_key)
+        random_key, body_key = jax.random.split(random_key)
+        body_genome_offspring, _ = body_mutation_fn(body_genome_parent, random_key)
+        nn_genome_offspring, random_key = nn_mutation_fn(nn_genome_parent, random_key)
+        offspring = nn_genome_offspring.copy({"body": body_genome_offspring})
+        return offspring, random_key
+
+    return _composed_mutation_fn
+
+
 def isoline_and_body_mutation(
         isoline_mutation_fn: Callable[[Genotype, Genotype, RNGKey], Tuple[Genotype, RNGKey]],
         body_mutation_fn: Callable[[Genotype, RNGKey], Tuple[Genotype, RNGKey]]
