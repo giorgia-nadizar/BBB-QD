3083517a51e551c57e8e352aa5a923c7c6202be2
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-09-23 11:34:38 +0200

    analysis updated

--- examples/centroids_locator.py
+++
@@ -1,9 +0,0 @@
-import plotly.express as px
-import jax.numpy as jnp
-
-base_path = f"../results/me/evo-body-10x10-floor-all_0"
-centroids = jnp.load(f"{base_path}/r3_centroids.npy")
-fitnesses = jnp.load(f"{base_path}/r3_fitnesses.npy")
-
-fig = px.scatter(x=centroids[:, 0], y=centroids[:, 1], color=fitnesses, text=jnp.arange(len(fitnesses)))
-fig.show()
