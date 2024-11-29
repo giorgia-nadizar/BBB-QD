60e4de3bac08d65a87d15a6e1bc9d55be65f35b0
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-11-29 14:59:53 +0100

    nn tracking updated

--- examples/nn_data_collection.py
+++ examples/nn_data_collection.py
@@ -1,13 +1,16 @@
 from functools import partial
 from pathlib import Path
-from typing import Callable, Union, Dict
+from typing import Callable, Union, Dict, Iterable, List
 
 import jax
 import jax.numpy as jnp
 import numpy as np
+import pandas as pd
 import yaml
 from flax.core import FrozenDict, frozen_dict
-from jax import jit
+from jax import jit, vmap
+from jax._src.flatten_util import ravel_pytree
+from pandas.core.interchange.dataframe_protocol import DataFrame
 from sklearn_extra.cluster import KMedoids
 
 from bbbqd.body.body_utils import compute_body_encoding_function
@@ -36,10 +39,9 @@ def collect_nn_experience(
 ) -> np.ndarray:
     # load config
     config = yaml.load(Path(f"{repertoire_path}/config.yaml").read_text(), Loader=yaml.FullLoader)
-    env = make_env(config)
 
     # Init policy network
-    policy_layer_sizes = config["policy_hidden_layer_sizes"] + (env.action_size,)
+    policy_layer_sizes = config["policy_hidden_layer_sizes"] + (1,)
     policy_network = MLP(
         layer_sizes=policy_layer_sizes,
         kernel_init=jax.nn.initializers.lecun_uniform(),
@@ -66,26 +68,74 @@ def collect_nn_experience(
     genotypes = repertoire.genotypes
     fitnesses = repertoire.fitnesses
 
+    obs_list = []
+
+    for geno_idx in range(len(fitnesses)):
+        # take only best genotype
+        current_genotype = _get_items_at_index(genotypes, geno_idx)
+        nn_genome, body_genome = current_genotype.pop("body")
+        controller = controller_creation_fn(_nn_policy_creation_fn(nn_genome))
+        body = body_encoding_fn(body_genome)
+
+        # Create tracking function
+        tracking_fn = partial(track_experience_controller_and_body, config=config)
+
+        _, observations = tracking_fn(controller, body)
+        obs_list.append(observations)
+
+    return np.vstack(obs_list)
+
+
+def eval_repertoire_on_data(
+        repertoire_path: str,
+        data_points: np.ndarray,
+) -> DataFrame:
+    # load config
+    config = yaml.load(Path(f"{repertoire_path}/config.yaml").read_text(), Loader=yaml.FullLoader)
+
+    # Init policy network
+    policy_layer_sizes = config["policy_hidden_layer_sizes"] + (1,)
+    policy_network = MLP(
+        layer_sizes=policy_layer_sizes,
+        kernel_init=jax.nn.initializers.lecun_uniform(),
+        final_activation=jnp.tanh,
+    )
+
+    n_inner_neurons = sum(config["policy_hidden_layer_sizes"])
+
+    def _nn_track_activation_fn(point: jnp.ndarray, policy_params: FrozenDict) -> jnp.ndarray:
+        _, inter_res = policy_network.apply(policy_params, point, capture_intermediates=True)
+        flatten_res, _ = ravel_pytree(inter_res)
+        activation_vals, _ = jnp.split(flatten_res, [n_inner_neurons])
+        return activation_vals
+
+    # load repertoire
+    reconstruction_fn = compute_reconstruction_fn(config, individual_id=False)
+    repertoire = GARepertoire.load(reconstruction_fn=reconstruction_fn, path=repertoire_path)
+
+    # extract genotypes
+    genotypes = repertoire.genotypes
+    fitnesses = repertoire.fitnesses
+
+    activations_dfs = []
     # take only best genotype
-    best_genotype = _get_items_at_index(genotypes, jnp.argmax(fitnesses))
-    nn_genome, body_genome = best_genotype.pop("body")
-    controller = controller_creation_fn(_nn_policy_creation_fn(nn_genome))
-    body = body_encoding_fn(body_genome)
+    for geno_idx in range(len(fitnesses)):
+        curr_genotype = _get_items_at_index(genotypes, geno_idx)
+        nn_genome, _ = curr_genotype.pop("body")
+        nn_function = partial(_nn_track_activation_fn, policy_params=nn_genome)
 
-    # Create tracking function
-    tracking_fn = partial(track_experience_controller_and_body, config=config)
+        # evaluate NN
+        jnp_data_points = jnp.asarray(data_points)
+        activations = vmap(nn_function)(jnp_data_points)
+        activations_df = pd.DataFrame.from_records(activations)
+        activations_df["point_id"] = list(range(len(data_points)))
+        activations_dfs.append(activations_df)
 
-    reward, observations = tracking_fn(controller, body)
-
-    return observations
+    return pd.concat(activations_dfs, ignore_index=True)
 
 
-if __name__ == '__main__':
-    n_data_points = 100
-
-    tasks = ["Walker-v0", "BridgeWalker-v0", "CustomCarrier-v0", "PlatformJumper-v0", "CaveCrawler-v0"]
-
-    seeds = range(10)
+def experiences_collection(tasks: Iterable[str], seeds: Iterable[int], n_data_points: int) -> List[str]:
+    files = []
     for task in tasks:
         task_name = task.replace("-v0", "").lower()
         task_obs = []
@@ -99,3 +149,34 @@ if __name__ == '__main__':
         representative_points = kmedoids.cluster_centers_
         print(f"Extracted {n_data_points} representative points for task {task_name}\n")
         np.save(f"../experiments/data/nn_data_{task_name}.npy", representative_points)
+        files.append(f"../experiments/data/nn_data_{task_name}.npy")
+    return files
+
+
+def activations_collection(tasks: Iterable[str], seeds: Iterable[int]) -> List[str]:
+    for task in tasks:
+        task_name = task.replace("-v0", "").lower()
+        task_dfs = []
+        data_points = np.load(f"../experiments/data/nn_data_{task_name}.npy")
+        for seed in seeds:
+            print(f"NN-{task_name}, {seed}")
+            rep_path = f"../results/ga/evo-body-nn-10x10-{task_name}_{seed}/"
+            df = eval_repertoire_on_data(rep_path, data_points)
+            df["seed"] = seed
+            task_dfs.append(df)
+        task_df = pd.concat(task_dfs, ignore_index=True)
+        task_df["task"] = task_name
+        task_df.to_csv(f"../experiments/data/nn_activations_{task_name}.tsv", index=False)
+
+
+if __name__ == '__main__':
+    file_names = experiences_collection(
+        n_data_points=100,
+        tasks=["Walker-v0", "BridgeWalker-v0", "CustomCarrier-v0", "PlatformJumper-v0", "CaveCrawler-v0"],
+        seeds=range(10),
+    )
+
+    df_names = activations_collection(
+        seeds=range(10),
+        tasks=["Walker-v0", "BridgeWalker-v0", "CustomCarrier-v0", "PlatformJumper-v0", "CaveCrawler-v0"],
+    )
