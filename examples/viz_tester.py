3c527457d8dbab2dfbe6cafac2e52bc04d0c765e
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2025-02-11 14:19:13 +0100

    custom envs added

--- examples/viz_tester.py
+++ examples/viz_tester.py
@@ -1,11 +1,12 @@
 import gym
 import evogym.envs
+import bbbqd.environments.extended_envs
 from evogym import sample_robot
 
 if __name__ == '__main__':
 
-    body, connections = sample_robot((5, 5))
-    env = gym.make('Carrier-v0', body=body)
+    body, connections = sample_robot((10, 10))
+    env = gym.make('CustomCarrier-v0', body=body)
     env.reset()
 
     for _ in range(1000):
